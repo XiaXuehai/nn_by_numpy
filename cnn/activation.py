9d5a3d0201cb659517c8ddbfc25f26f8e4d60bb2
xiaxuehai <609632889@qq.com>
2018-11-23 11:53:21 +0800

    add leaky relu

--- cnn/activation.py
+++ cnn/activation.py
@@ -2,16 +2,6 @@
 
 import numpy as np
 
-def relu_forward(x):
-    out = np.maximum(x, 0) #compare with 0
-    cache = x
-    return out, x
-
-def relu_backward(dout, cache):
-    dX = dout.copy()
-    dX[cache<=0] = 0
-    return dX
-
 def fc_forward(X, W, b):
     out = X @ W + b # np.matmul(x, w)
     cache = (W, X)
@@ -25,6 +15,30 @@ def fc_backward(dout, cache):
     dX = dout @ w.T
 
     return dX, dW, db
+
+def relu_forward(x):
+    out = np.maximum(x, 0) #compare with 0
+    cache = x
+    return out, x
+
+def relu_backward(dout, cache):
+    dX = dout.copy()
+    dX[cache<=0] = 0
+    return dX
+
+# leaky relu
+def lrelu_forward(x, a=1e-3):
+    out = np.maximum(a*x, x)
+    cache = (x, a)
+    return out, cache
+
+def lrelu_backward(dout, cache):
+    x, a = cache
+    dx = dout.copy()
+    dx[x<0] *= a
+    return dx
+
+
 
 if __name__ == '__main__':
     pass
